833319024a14cb0f04711fcf999c241cc7393bcb
promind4 <promind303@gmail.com>
2025-11-24 03:17:34 +0100

    Refactor rate limiter by removing empty config
    Removed unused Config initialization in rate limiter.

--- windsurf/src/core/rate_limiter.py
+++ windsurf/src/core/rate_limiter.py
@@ -1,15 +1,13 @@
 from slowapi import Limiter
 from slowapi.util import get_remote_address
-from starlette.config import Config
-
-empty_config = Config(environ={})
 
 limiter = Limiter(
     key_func=get_remote_address,
     default_limits=["1000/minute"],
-    enabled=True,
-    config=empty_config
+    enabled=True
 )
+
+
 
 
 
