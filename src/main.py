a87929b2d208981091e646f8f8e38089d635bc8f
azura <promind303@gmail.com>
2025-11-24 10:03:54 +0100

    fix: update CORS config for Vercel and localhost

--- src/main.py
+++ src/main.py
@@ -21,7 +21,10 @@ app = FastAPI(
 # Configuration CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex='https://.*\.vercel\.app',
+    # Cette Regex autorise https://nimporte-quoi.vercel.app
+    allow_origin_regex="https://.*\.vercel\.app",
+    # On garde localhost pour le dev
+    allow_origins=["http://localhost:5173", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
